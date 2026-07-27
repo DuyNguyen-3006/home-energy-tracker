72c209c971091033fb3faa8a9d32de7751a55c2b
duynp <duynguyen04302004@gmail.com>
2026-07-27 09:07:14 +0700

    ingest ingestion service

---
+++ ingestion-service/src/main/java/com/andrew/ingestion_service/application/service/IngestionService.java
@@ -0,0 +1,29 @@
+package com.andrew.ingestion_service.application.service;
+
+import com.andrew.ingestion_service.application.dto.EnergyUsageDto;
+import com.andrew.ingestion_service.infrastructure.kafka.event.EnergyUsageEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class IngestionService {
+
+    private final KafkaTemplate<String, EnergyUsageEvent> kafkaTemplate;
+
+    public IngestionService(KafkaTemplate<String, EnergyUsageEvent> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void ingestEnergyUsage(EnergyUsageDto request) {
+        EnergyUsageEvent event = EnergyUsageEvent.builder()
+            .deviceId(request.deviceId())
+            .energyConsumed(request.energyConsumed())
+            .timestamp(request.timestamp())
+            .build();
+
+        kafkaTemplate.send("energy-usage", event);
+        log.info("Ingesty energy usage event: {}", event);
+    }
+}
